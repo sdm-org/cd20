c7b8a28a04080b858bdd3d748096ab73c9a5ecfb
Christian Dupuis <cd@atomist.com>
2018-05-15 17:47:40 +0200

    update package script

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -6,9 +6,8 @@ timestamp=`date "+%Y%m%d%H%M%S"`
 
 cd source-code/
   ./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-${timestamp} versions:commit
-  project_version=$(./mvnw help:evaluate -Dexpression=project.version | grep -v "^\[")
-  echo version is $project_version
   ./mvnw clean package deploy --settings .settings.xml
+  project_version=$(grep -E -m 1 -o "<version>(.*)</version>" pom.xml | sed 's/<version>//' | sed 's/<\/version>//')
 cd ..
 
 echo $project_version > build-output/version
