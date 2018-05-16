1e7c30f6bf9adc42bb907689086484b2b9159574
Christian Dupuis <cd@atomist.com>
2018-05-16 02:12:26 +0200

    update

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -5,8 +5,8 @@ set -e -u -x
 timestamp=`date "+%Y%m%d%H%M%S"`
 
 cd source-code/
-  ./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-${timestamp} versions:commit
-  ./mvnw clean package deploy --settings .settings.xml
+  ./mvnw -Dmaven.repo.local=./.m2/repository build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-${timestamp} versions:commit
+  ./mvnw -Dmaven.repo.local=./.m2/repository clean package deploy --settings .settings.xml
   project_version=$(grep -E -m 1 -o "<version>(.*)</version>" pom.xml | sed 's/<version>//' | sed 's/<\/version>//')
 cd ..
 
